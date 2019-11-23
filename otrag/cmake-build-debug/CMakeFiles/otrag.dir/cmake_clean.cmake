5b41772503403727482ce044890ccd874c01c7ab
VadimYunu <58107187+VadimYunu@users.noreply.github.com>
2019-11-23 17:58:59 +0700

    Add files via upload

---
+++ otrag/cmake-build-debug/CMakeFiles/otrag.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/otrag.dir/main.cpp.obj"
+  "libotrag.dll.a"
+  "otrag.exe"
+  "otrag.exe.manifest"
+  "otrag.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/otrag.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
